d891d956e5ce9430de1ce7f6e8e9b6a4ee3b1ca9
donnovan22 <val22002@byui.edu>
2023-01-09 00:34:28 -0500

    new route added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
+  res.send('Donnovan Valencia');
+});
+routes.get('/test', (req, res) => {
   res.send('Sarah Birch');
 });
 
